066af19c0128647c39359d1318c523946e4a5f13
Miguel <cmiguelmg@gmail.com>
2016-04-20 22:53:28 -0500

    Messages GET and POST for user

--- src/main/java/com/logicware/controllers/UserRestController.java
+++ src/main/java/com/logicware/controllers/UserRestController.java
@@ -6,9 +6,11 @@ package com.logicware.controllers;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +35,11 @@ public class UserRestController {
 	@RequestMapping(value="/add", method=RequestMethod.POST)
 	User add(@RequestBody User input){
 		return userRepository.save(input);
+	}
+	
+	@RequestMapping(value="getByCorreo/{correo}", method=RequestMethod.GET)
+	User getByCorreo(@PathVariable String correo){
+		return userRepository.findByCorreo(correo);
 	}
 
 }
--- src/main/java/com/logicware/main/BrWebServiceSpringApplication.java
+++ src/main/java/com/logicware/main/BrWebServiceSpringApplication.java
@@ -4,9 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.logicware.entities.User;
+import com.logicware.repositories.UserRepository;
+
+@EnableWebMvc
 @SpringBootApplication
+@EnableJpaRepositories(basePackages="com.logicware.repositories")
+@EntityScan(basePackages={"com.logicware.entities"})
+@ComponentScan(basePackages={"com.logicware.controllers", "com.logicware.repositories", "com.logicware.entities"})
 public class BrWebServiceSpringApplication {
 
 	public static void main(String[] args) {
@@ -14,11 +25,14 @@ public class BrWebServiceSpringApplication {
 	}
 }
 
-@Component
+/*@Component
 class CommandLineRun implements CommandLineRunner{
+	
+	@Autowired UserRepository userRepository;
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		// Nothing
+		for(User u: userRepository.findAll())
+			System.out.println(u.getCorreo());
 	}
-}
+}*/
