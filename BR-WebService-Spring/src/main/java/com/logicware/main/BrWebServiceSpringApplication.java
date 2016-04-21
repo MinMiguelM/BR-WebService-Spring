ffb70953ae4a7b1fc07808898fb12f056c4e8274
Miguel <cmiguelmg@gmail.com>
2016-04-20 21:58:42 -0500

    first commit

---
+++ BR-WebService-Spring/src/main/java/com/logicware/main/BrWebServiceSpringApplication.java
@@ -0,0 +1,12 @@
+package com.logicware.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrWebServiceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrWebServiceSpringApplication.class, args);
+	}
+}
---
+++ BR-WebService-Spring/src/test/java/com/logicware/main/BrWebServiceSpringApplicationTests.java
@@ -0,0 +1,18 @@
+package com.logicware.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BrWebServiceSpringApplication.class)
+@WebAppConfiguration
+public class BrWebServiceSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
